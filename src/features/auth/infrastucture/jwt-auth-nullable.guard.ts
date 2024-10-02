f794205525671e8bdf526f5edd12017dc652ccd3
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-02 13:36:10 -0300

    Updated JwtAuthNullableGuard

--- src/features/auth/infrastucture/jwt-auth-nullable.guard.ts
+++ src/features/auth/infrastucture/jwt-auth-nullable.guard.ts
@@ -1,15 +1,20 @@
 import {Injectable, CanActivate, ExecutionContext} from '@nestjs/common';
 import {JwtService} from '@nestjs/jwt';
 import {Request} from 'express';
-import {jwtAccessConstants} from "../constants";
+import {ConfigService} from "@nestjs/config";
 
 
 @Injectable()
 export class JwtAuthNullableGuard implements CanActivate {
-    constructor(private jwtService: JwtService) {
+    constructor(private jwtService: JwtService,
+                private configService: ConfigService) {
     }
 
     canActivate(context: ExecutionContext): boolean {
+        const accessSecret = this.configService.get<string>('jwtSettings.JWT_ACCESS_SECRET');
+        const accessExpiry = this.configService.get<string>('jwtSettings.ACCESS_TOKEN_EXPIRY');
+
+
         const request = context.switchToHttp().getRequest<Request>();
         const authHeader = request.headers.authorization;
 
@@ -20,8 +25,8 @@ export class JwtAuthNullableGuard implements CanActivate {
 
         const token = authHeader.split(' ')[1];
         try {
-            const decoded = this.jwtService.verify(token, {secret: jwtAccessConstants.jwt_secret});
-            request['userId'] = decoded.id;  // Set userId to the decoded token's id
+            const decoded = this.jwtService.verify(token, {secret: accessSecret});
+            request['userId'] = decoded.id;
             return true;
         } catch (error) {
 

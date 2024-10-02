c534124d2683946b6a1c70854c50c7e8c1ec3dfd
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-02 13:36:28 -0300

    Updated JwtAuthGuard

--- src/infrastructure/guards/jwt-auth.guard.ts
+++ src/infrastructure/guards/jwt-auth.guard.ts
@@ -1,16 +1,19 @@
 import {Injectable, CanActivate, ExecutionContext, UnauthorizedException} from '@nestjs/common';
 import {JwtService} from '@nestjs/jwt';
 import {Request} from 'express';
-import {jwtAccessConstants} from "../../features/auth/constants";
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-    constructor(private jwtService: JwtService) {
+    constructor(private jwtService: JwtService,
+                private configService: ConfigService) {
     }
 
     canActivate(context: ExecutionContext): boolean {
         const request = context.switchToHttp().getRequest<Request>();
         const authHeader = request.headers.authorization;
+        const accessSecret = this.configService.get<string>('jwtSettings.JWT_ACCESS_SECRET');
+        const accessExpiry = this.configService.get<string>('jwtSettings.ACCESS_TOKEN_EXPIRY');
 
         if (!authHeader) {
             throw new UnauthorizedException('Authorization header is missing');
@@ -18,10 +21,19 @@ export class JwtAuthGuard implements CanActivate {
 
         const token = authHeader.split(' ')[1];
         try {
-            const decoded = this.jwtService.verify(token, {secret: jwtAccessConstants.jwt_secret});
+
+            const decoded = this.jwtService.verify(token, {secret: accessSecret});
             request['userId'] = decoded.id;
             return true;
+
         } catch (error) {
+
+            if (Error.name === 'TokenExpiredError') {
+
+                throw new UnauthorizedException('Token has expired');
+
+            }
+
             throw new UnauthorizedException('Invalid token');
         }
     }

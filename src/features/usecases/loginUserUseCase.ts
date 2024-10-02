ef30160c1b02ac66699d3f676fd13b4927197167
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-02 13:35:50 -0300

    Updated LoginUserUseCase

--- src/features/usecases/loginUserUseCase.ts
+++ src/features/usecases/loginUserUseCase.ts
@@ -3,9 +3,9 @@ import {UsersRepository} from "../users/infrastructure/users.repository";
 import {UsersQueryRepository} from "../users/infrastructure/users.query-repository";
 import {UserLoginDto} from "../auth/api/models/input/login-user.input.dto";
 import {UnauthorizedException} from "@nestjs/common";
-import {jwtRefreshConstants} from "../auth/constants";
 import bcrypt from "bcrypt";
 import {JwtService} from "@nestjs/jwt";
+import {ConfigService} from "@nestjs/config";
 
 
 export class LoginUserUseCaseCommand {
@@ -24,10 +24,18 @@ export class LoginUserUseCase implements ICommandHandler<LoginUserUseCaseCommand
         private usersRepository: UsersRepository,
         private usersQueryRepository: UsersQueryRepository,
         private jwtService: JwtService,
+        private configService: ConfigService
     ) {
     }
 
     async execute(command: LoginUserUseCaseCommand) {
+
+        const accessSecret = this.configService.get<string>('jwtSettings.JWT_ACCESS_SECRET');
+        const accessExpiry = this.configService.get<string>('jwtSettings.ACCESS_TOKEN_EXPIRY');
+
+        const refreshSecret = this.configService.get<string>('jwtSettings.JWT_REFRESH_SECRET');
+        const refreshExpiry = this.configService.get<string>('jwtSettings.REFRESH_TOKEN_EXPIRY');
+
 
         const user = await this.validateUser(
             command.loginDto.loginOrEmail,
@@ -39,7 +47,7 @@ export class LoginUserUseCase implements ICommandHandler<LoginUserUseCaseCommand
 
         const payload = {loginOrEmail: command.loginDto.loginOrEmail, id: user.id};
 
-        const accessToken = this.jwtService.sign(payload)
+        const accessToken = this.jwtService.sign(payload, {});
 
         const refreshToken = this.jwtService.sign({
             id: user.id,
@@ -47,7 +55,7 @@ export class LoginUserUseCase implements ICommandHandler<LoginUserUseCaseCommand
             userDevice: command.userDevice,
             userAgent: command.userAgent
 
-        }, {secret: jwtRefreshConstants.jwt_secret, expiresIn: jwtRefreshConstants.refresh_token_expiry});
+        }, {secret: refreshSecret, expiresIn: refreshExpiry});
 
         return {accessToken, refreshToken};
 

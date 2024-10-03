7e4c9409e64c69d33148758b048ddb3e340bb066
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-03 15:02:24 -0300

    Added createSession method

--- src/features/session/infrastructure/session.repository.ts
+++ src/features/session/infrastructure/session.repository.ts
@@ -1,12 +1,19 @@
-import {Injectable, Session} from "@nestjs/common";
+import {Injectable} from "@nestjs/common";
 import {InjectModel} from "@nestjs/mongoose";
 import {Model} from "mongoose";
-import {SessionDocument} from "../domain/session.entity";
+import {Session, SessionDocument} from "../domain/session.entity";
+
 
 @Injectable()
 export class SessionRepository {
     constructor(@InjectModel(Session.name) private sessionModel: Model<SessionDocument>) {
     }
 
+    //TODO
+    async createSession(sessionUser: any) {
 
+        const result = await this.sessionModel.create(sessionUser);
+
+        return result._id.toString();
+    }
 }

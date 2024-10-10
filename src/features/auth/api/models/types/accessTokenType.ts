f1da91ab9fd7fe8a64ae3c9ba214d87139e34549
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-10 16:29:01 -0300

    Added types: added RefreshTokenType

--- src/features/auth/api/models/types/accessTokenType.ts
+++ src/features/auth/api/models/types/accessTokenType.ts
@@ -2,3 +2,10 @@ export type AccessTokenType = {
     loginOrEmail: string | undefined,
     id: string
 }
+
+export type RefreshTokenType = {
+    userId: string,
+    deviceId: string,
+    userIP: string,
+    userAgent: string,
+}

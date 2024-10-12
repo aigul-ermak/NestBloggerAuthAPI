6853daef617d9276e9d199060e3cedac35d31638
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-12 18:42:19 -0300

    Updated SendNewCodeToEmailUseCase: updated type

--- src/features/auth/api/usecases/sendNewCodeToEmailUseCase.ts
+++ src/features/auth/api/usecases/sendNewCodeToEmailUseCase.ts
@@ -4,13 +4,13 @@ import {UsersQueryRepository} from "../../../users/infrastructure/users.query-re
 import {CommandHandler, ICommandHandler} from "@nestjs/cqrs";
 import {v4 as uuidv4} from "uuid";
 import {EmailService} from "../../../email/email.service";
-import {ResendEmailDto} from "../../../email/models/input/email.input.dto";
 import {UserWithIdOutputModel} from "../../../users/api/models/output/user.output.model";
+import {EmailDto} from "../../../email/models/input/email.input.dto";
 
 
 export class SendNewCodeToEmailUseCaseCommand {
 
-    constructor(public resendEmailDto: ResendEmailDto) {
+    constructor(public resendEmailDto: EmailDto) {
     }
 }
 

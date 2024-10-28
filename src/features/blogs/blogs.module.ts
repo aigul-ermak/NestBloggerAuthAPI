eb4453c9c766e0fa92fba06dd30ee742ebd54764
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-28 19:03:04 -0300

    Updated blog module

--- src/features/blogs/blogs.module.ts
+++ src/features/blogs/blogs.module.ts
@@ -6,13 +6,30 @@ import {MongooseModule} from '@nestjs/mongoose';
 import {Blog, BlogEntity} from './domain/blog.entity';
 import {BlogsQueryRepository} from "./infrastructure/blogs.query-repository";
 import {IsBlogByIdExistsConstraint} from "../../infrastructure/decorators/validation/blog-is-exist.decorator";
+import {CqrsModule} from "@nestjs/cqrs";
+import {CreateBlogUseCase} from "./api/usecases/createBlogUseCase";
+import {GetBlogByIdUseCase} from "./api/usecases/getBlogByIdUseCase";
+import {GetAllBlogsUseCase} from "./api/usecases/getAllBlogsUseCase";
+import {DeleteBlogByIdUseCase} from "./api/usecases/deleteBlogByIdUseCase";
+import {UpdateBlogUseCase} from "./api/usecases/updateBlogUseCase";
+import {CreatePostUseCase} from "../posts/api/usecases/createPostUseCase";
+import {PostsModule} from "../posts/posts.module";
+import {PostsQueryRepository} from "../posts/infrastructure/posts.query-repository";
+import {LikesQueryRepository} from "../likePost/infrastructure/likes.query-repository";
+import {LikesModule} from "../likePost/likes.module";
 
+const useCases = [CreateBlogUseCase, GetBlogByIdUseCase, GetAllBlogsUseCase,
+    DeleteBlogByIdUseCase, UpdateBlogUseCase, CreatePostUseCase,]
 
 @Module({
     imports: [
         MongooseModule.forFeature([{name: Blog.name, schema: BlogEntity}]),
+        PostsModule,
+        LikesModule,
+        CqrsModule,
     ],
-    providers: [BlogsService, BlogsRepository, BlogsQueryRepository, IsBlogByIdExistsConstraint],
+    providers: [BlogsService, BlogsRepository, BlogsQueryRepository,
+        IsBlogByIdExistsConstraint, PostsQueryRepository, LikesQueryRepository, ...useCases],
     exports: [BlogsRepository, BlogsQueryRepository]
 })
 export class BlogsModule {

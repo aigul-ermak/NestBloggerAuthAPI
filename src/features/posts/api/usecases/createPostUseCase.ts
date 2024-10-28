cf74861040919b4e382ff94e4457de3775c79d28
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-28 18:48:19 -0300

    Updated CreatePostUseCase

--- src/features/posts/api/usecases/createPostUseCase.ts
+++ src/features/posts/api/usecases/createPostUseCase.ts
@@ -4,8 +4,12 @@ import {BlogsQueryRepository} from "../../../blogs/infrastructure/blogs.query-re
 import {NotFoundException} from "@nestjs/common";
 import {CreatePostForBlogInputDto} from "../models/input/create-post.input.dto";
 import {PostsQueryRepository} from "../../infrastructure/posts.query-repository";
-import {PostsOutputModelMapper} from "../models/output/post-db.output.model";
 import {LikesQueryRepository} from "../../../likePost/infrastructure/likes.query-repository";
+import {CreatePostMdOutputType} from "../models/types/output/createPostMdOutputType";
+import {PostMdOutputType} from "../models/types/output/postMdOutputType";
+import {BlogMdOutputType} from "../../../blogs/api/models/types/createBlogMdOutputType";
+import {PostToBlogInputType} from "../models/types/input/createPostToBlogInputType";
+import {PostLikeOutputModelMapper} from "../models/output/post-db.output.model";
 
 
 export class CreatePostUseCaseCommand {
@@ -27,30 +31,31 @@ export class CreatePostUseCase implements ICommandHandler<CreatePostUseCaseComma
 
     async execute(command: CreatePostUseCaseCommand) {
 
-        const blog = await this.blogsQueryRepository.getBlogById(command.post.blogId);
+        const blog: BlogMdOutputType | null = await this.blogsQueryRepository.getBlogById(command.post.blogId);
 
         if (!blog) {
             throw new NotFoundException(`Blog not found`);
         }
 
-        const newCreatePost = {
+        const newCreatePost: PostToBlogInputType = {
             ...command.post,
             blogName: blog.name,
             createdAt: new Date(Date.now()),
         }
 
-        const createdPost = await this.postsRepository.insert(newCreatePost);
+        const createdPost: CreatePostMdOutputType = await this.postsRepository.insert(newCreatePost);
 
-        const post = await this.postsQueryRepository.getPostById(createdPost.id);
+        const post: PostMdOutputType = await this.postsQueryRepository.getPostById(createdPost.id);
 
-        if (post === null) {
+        if (!post) {
             throw new NotFoundException(`Post not found`);
         }
 
+        //TODO
         const newestLikes = await this.likesQueryRepository.getNewestLikesForPost(post.id);
-
+        console.log("newestLikes", newestLikes)
         const status = 'None';
 
-        return PostsOutputModelMapper(post, newestLikes, status);
+        return PostLikeOutputModelMapper(post, newestLikes, status);
     }
 }

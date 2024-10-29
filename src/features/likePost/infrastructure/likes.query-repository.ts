8b63fb7fff3af3bfbe66f69e1c84ec32df0a8981
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-29 10:59:37 -0300

    Updated LikesQueryRepository

--- src/features/likePost/infrastructure/likes.query-repository.ts
+++ src/features/likePost/infrastructure/likes.query-repository.ts
@@ -9,25 +9,21 @@ export class LikesQueryRepository {
     constructor(@InjectModel(Like.name) private likeModel: Model<LikeDocument>) {
     }
 
-    async getLike(parentId: string, userId: string) {
+    async getLike(parentId: string, userId: string): Promise<LikeDocument | null> {
         return this.likeModel.findOne({parentId: parentId, userId: userId});
     }
 
-    async getNewestLikesForPost(postId: string) {
+    async getNewestLikesForPost(postId: string): Promise<LikeDocument[]> {
         const newestLikes = await this.likeModel.find({parentId: postId, status: LIKE_STATUS.LIKE})
             .sort({createdAt: -1})
             .limit(3)
             .lean();
 
-        return newestLikes.map(like => ({
-            addedAt: like.createdAt,
-            userId: like.userId,
-            login: like.login,
-        }));
+        return newestLikes as LikeDocument[];
     }
 
 
-    async checkLike({parentId, userId}: { parentId: string, userId: string }) {
+    async checkLike({parentId, userId}: { parentId: string, userId: string }): Promise<boolean> {
         const res = await this.likeModel.findOne({parentId: parentId, userId: userId}).lean();
         return !!res;
     }

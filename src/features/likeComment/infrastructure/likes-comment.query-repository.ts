132d26f41356bffd1e766d36e9a780de2c434ebb
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-29 09:22:52 -0300

    Updated like to comment repo

--- src/features/likeComment/infrastructure/likes-comment.query-repository.ts
+++ src/features/likeComment/infrastructure/likes-comment.query-repository.ts
@@ -2,18 +2,19 @@ import {Injectable} from "@nestjs/common";
 import {InjectModel} from "@nestjs/mongoose";
 import {LikeComment, LikeCommentDocument} from "../domain/like-comment.entity";
 import {Model} from "mongoose";
+import {CommentDocument} from "../../comments/domain/comment.entity";
 
 @Injectable()
 export class LikesCommentQueryRepository {
     constructor(@InjectModel(LikeComment.name) private likeCommentModel: Model<LikeCommentDocument>) {
     }
 
-    async getLike(commentId: string, userId: string) {
-        return this.likeCommentModel.findOne({commentId: commentId, userId: userId});
+    async getLike(commentId: string, userId: string): Promise<LikeCommentDocument | null> {
+        return this.likeCommentModel.findOne({commentId, userId}) as unknown as LikeCommentDocument;
     }
 
-    async checkLike({commentId, userId}: { commentId: string, userId: string }) {
-        const res = await this.likeCommentModel.findOne({commentId: commentId, userId: userId}).lean();
+    async checkLike({commentId, userId}: { commentId: string, userId: string }): Promise<boolean> {
+        const res = await this.likeCommentModel.findOne({commentId, userId}).lean();
         return !!res;
     }
 }

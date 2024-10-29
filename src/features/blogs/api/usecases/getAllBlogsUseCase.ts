00ce98ed1a3a50e8eddb58b7f445177e1d39f178
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-29 09:27:50 -0300

    Updated GetAllBlogsUseCase: added types

--- src/features/blogs/api/usecases/getAllBlogsUseCase.ts
+++ src/features/blogs/api/usecases/getAllBlogsUseCase.ts
@@ -1,7 +1,9 @@
 import {BlogsQueryRepository} from "../../infrastructure/blogs.query-repository";
 import {SortBlogsDto} from "../models/input/sort-blog.input.dto";
-import {BlogOutputModelMapper} from "../models/output/blog.output.model";
+import {BlogOutputModelMapper} from "../models/output/blogOutputModel";
 import {CommandHandler, ICommandHandler} from "@nestjs/cqrs";
+import {BlogMdOutputType} from "../models/types/createBlogMdOutputType";
+import {GetAllBlogOutputType} from "../models/types/getAllBlogOutputType";
 
 
 export class GetAllBlogsUseCaseCommand {
@@ -14,12 +16,12 @@ export class GetAllBlogsUseCase implements ICommandHandler<GetAllBlogsUseCaseCom
     constructor(private blogsQueryRepository: BlogsQueryRepository) {
     }
 
-    async execute(command: GetAllBlogsUseCaseCommand) {
-        const sortBy = command.sortData.sortBy ?? 'createdAt';
-        const sortDirection = command.sortData.sortDirection ?? 'desc';
-        const pageNumber = command.sortData.pageNumber ?? 1;
-        const pageSize = command.sortData.pageSize ?? 10;
-        const searchNameTerm = command.sortData.searchNameTerm ?? null;
+    async execute(command: GetAllBlogsUseCaseCommand): Promise<GetAllBlogOutputType> {
+        const sortBy: string = command.sortData.sortBy ?? 'createdAt';
+        const sortDirection: "asc" | "desc" = command.sortData.sortDirection ?? 'desc';
+        const pageNumber: number = command.sortData.pageNumber ?? 1;
+        const pageSize: number = command.sortData.pageSize ?? 10;
+        const searchNameTerm: string | null = command.sortData.searchNameTerm ?? null;
 
 
         let filter: any = {};
@@ -28,7 +30,7 @@ export class GetAllBlogsUseCase implements ICommandHandler<GetAllBlogsUseCaseCom
             filter['$or'] = [{
                 'name': {
                     $regex: searchNameTerm,
-                    $options: 'i' // case-insensitive search
+                    $options: 'i'
                 }
             }];
         }
@@ -37,10 +39,12 @@ export class GetAllBlogsUseCase implements ICommandHandler<GetAllBlogsUseCaseCom
             filter = {};
         }
 
-        const blogs = await this.blogsQueryRepository
-            .findAllBlogsByFilter(filter, sortBy, sortDirection, (pageNumber - 1) * pageSize, pageSize);
-        const totalCount = await this.blogsQueryRepository.countDocuments(filter);
-        const pageCount = Math.ceil(totalCount / pageSize);
+        const blogs: BlogMdOutputType[] = (await this.blogsQueryRepository
+            .findAllBlogsByFilter(filter, sortBy, sortDirection, (pageNumber - 1) * pageSize, pageSize))
+            .map((blog) => blog.toObject() as BlogMdOutputType);
+        const totalCount: number = await this.blogsQueryRepository.countDocuments(filter);
+        const pageCount: number = Math.ceil(totalCount / pageSize);
+
 
         return {
             pagesCount: pageCount,

8354d89255c2982c7fce11d8bd9dfe56ae50113f
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-29 09:22:22 -0300

    Updated post repo

--- src/features/posts/infrastructure/posts.query-repository.ts
+++ src/features/posts/infrastructure/posts.query-repository.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
 import {Model, SortOrder} from 'mongoose';
 import {Post, PostDocument} from '../domain/posts.entity';
+import {PostMdOutputType} from "../api/models/types/output/postMdOutputType";
 
 
 @Injectable()
@@ -23,12 +24,11 @@ export class PostsQueryRepository {
         sortDirection: string,
         skip: number,
         limit: number
-    ) {
+    ): Promise<PostDocument[]> {
         const sortOrder = sortDirection === 'desc' ? -1 : 1;
 
         const result = await this.postModel
             .find()
-            .lean()
             .sort({[sortBy]: sortOrder})
             .skip(skip)
             .limit(limit)
@@ -42,7 +42,7 @@ export class PostsQueryRepository {
     }
 
     async findByBlogId(blogId: string): Promise<PostDocument[]> {
-        return this.postModel.find({blogId}).exec();
+        return this.postModel.find({blogId});
     }
 
     async countByBlogId(blogId: string) {
@@ -62,12 +62,13 @@ export class PostsQueryRepository {
             [sort]: sortDirection === 'asc' ? 1 : -1,
         };
 
-        return this.postModel
+        const res: PostDocument[] = await this.postModel
             .find({blogId})
             .sort(sortOption)
             .skip(skip)
             .limit(pageSize)
             .exec()
 
+        return res as PostDocument[];
     }
 }

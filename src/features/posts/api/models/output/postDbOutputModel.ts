795a39799dd5d1ff7f9a375b5968939c5eaae326
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-30 11:06:24 -0300

    Updated PostOutputModel

--- src/features/posts/api/models/output/postDbOutputModel.ts
+++ src/features/posts/api/models/output/postDbOutputModel.ts
@@ -7,20 +7,24 @@ export class PostOutputModel {
     content: string;
     blogId: string;
     blogName: string;
-    createdAt: Date;
+    createdAt: number;
     extendedLikesInfo: {
         likesCount: number,
         dislikesCount: number,
         myStatus: string,
         newestLikes: {
-            addedAt: Date;
+            createdAt: number;
             userId: string;
             login: string;
         }[],
     };
 }
 
-export const PostLikeOutputModelMapper = (post: PostDocument, newestLikes: any[], status: string): PostOutputModel => {
+export const PostLikeOutputModelMapper = (post: PostDocument, newestLikes: {
+    createdAt: Date;
+    login: string;
+    userId: string
+}[], status: string): PostOutputModel => {
     const outputModel: PostOutputModel = new PostOutputModel();
 
     outputModel.id = post._id.toString();
@@ -29,14 +33,14 @@ export const PostLikeOutputModelMapper = (post: PostDocument, newestLikes: any[]
     outputModel.content = post.content;
     outputModel.blogId = post.blogId;
     outputModel.blogName = post.blogName;
-    outputModel.createdAt = post.createdAt;
+    outputModel.createdAt = +post.createdAt;
 
     outputModel.extendedLikesInfo = {
         likesCount: post.likesCount,
         dislikesCount: post.dislikesCount,
         myStatus: status,
         newestLikes: newestLikes.map(like => ({
-            addedAt: like.addedAt,
+            createdAt: +like.createdAt,
             userId: like.userId,
             login: like.login
         }))
